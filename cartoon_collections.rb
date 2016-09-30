28ac43e248a638476ab8acc709b6571c2d14f361
Kevin Evans <mkevinevans@gmail.com>
2016-09-30 20:52:01 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.each_with_index do |value, index|
+    puts "#{index+1}. #{value}"
+    end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  array.collect{|n| n.capitalize+"!"}
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.any? { |word| word.length > 4 }
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |x| cheese_types.include?(x)
+  # binding.pry
+}
+
+  # the array below is here to help
 end
